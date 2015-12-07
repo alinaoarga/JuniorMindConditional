44a5d20f4a19828efc21310583aafa2f048b5fe5
alinaoarga <oarga_alina@yahoo.com>
2015-12-07 15:19:50 +0200

    First WatermelonTest

--- Watermelon/WatermelonTests.cs
+++ Watermelon/WatermelonTests.cs
@@ -4,11 +4,26 @@ using Microsoft.VisualStudio.TestTools.UnitTesting;
 namespace Watermelon
 {
     [TestClass]
-    public class UnitTest1
+    public class WatermelonTests
     {
         [TestMethod]
-        public void TestMethod1()
+        public void FirstWatermelonTest()
         {
+            Assert.AreEqual ("DA",CalculatePossibility(20));
+            
+        }
+        string CalculatePossibility(double WatermelonWeight)
+        {
+            string str1 = "DA";
+            string str2 = "NU";
+            if (WatermelonWeight % 2 == 0)
+            {
+                return str1;
+            }
+            else
+            {
+                return str2;
+            }
         }
     }
 }
