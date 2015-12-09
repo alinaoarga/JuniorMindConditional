73fddd4079dfde93d5194d826025bfed5fdf0e58
alinaoarga <oarga_alina@yahoo.com>
2015-12-09 10:25:03 +0200

    Third Penalty Test

--- Arrears/ArrearsTests.cs
+++ Arrears/ArrearsTests.cs
@@ -9,18 +9,48 @@ namespace Arrears
         [TestMethod]
         public void FirstPenaltyTest()
         {
-            Assert.AreEqual(104, CalculateArrears(2, 100));
+            Assert.AreEqual(104, CalculateArrears(2, 100, 2));
         }
         [TestMethod]
         public void SecondPenaltyTest()
         {
-            Assert.AreEqual(155, CalculateArrears(11, 100));
+            Assert.AreEqual(155, CalculateArrears(11, 100, 5));
         }
-        decimal CalculateArrears(int days, decimal rent)
+        [TestMethod]
+        public void ThirdPenaltyTest()
         {
-           decimal firstTypeOfRent = rent + days * (2 * rent / 100);
-            decimal secondTypeOfRent = rent + days * (5 * rent / 100);
-            return days < 10 ? firstTypeOfRent : secondTypeOfRent;
+            Assert.AreEqual(420, CalculateArrears(32, 100, 10));
+        }
+        decimal CalculateArrears(int days, decimal rent, decimal percent)
+        {
+            decimal typeOfRent = rent + days * (percent * rent / 100);
+            if (AreUpToTenDays(days))
+            {
+                percent = 2;
+                return typeOfRent;
+            }
+            else if (AreUpToThirtyDays(days))
+            {
+                percent = 5;
+                return typeOfRent;
+            }
+            else if (AreUpToFortyDays(days))
+                percent = 10;
+            return typeOfRent;
+        }
+        private bool AreUpToFortyDays(int days)
+        {
+            return days < 40;
+        }
+
+        private bool AreUpToThirtyDays(int days)
+        {
+            return days < 30;
+        }
+
+        private static bool AreUpToTenDays(int days)
+        {
+            return days < 10;
         }
     }
 }
