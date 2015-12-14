80edc5ec08cbcfa40f38f06945029795a3629f31
alinaoarga <oarga_alina@yahoo.com>
2015-12-14 12:08:37 +0200

    Add empty RomanNumbers project

---
+++ RomanNumbers/RomanNumbersTests.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace RomanNumbers
+{
+    [TestClass]
+    public class RomanNumbersTests
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
